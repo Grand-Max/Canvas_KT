ac963e98a31dfe3b9a9b2e3cc1e0f7cb05ce9148
Max <49816300+Grand-Max@users.noreply.github.com>
2020-08-07 17:06:29 +0300

    canvas init

--- app/src/main/java/com/example/canvas_kt/MainActivity.kt
+++ app/src/main/java/com/example/canvas_kt/MainActivity.kt
@@ -1,12 +1,35 @@
 package com.example.canvas_kt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+
+    var drawView : DrawView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        
+        makeCanvas()
     }
+
+    fun makeCanvas(){
+        drawView = DrawView(this)
+        setContentView(drawView)
+    }
+
+
+    class DrawView(ct : Context) : View(ct)  {
+        override fun onDraw(canvas: Canvas?) {
+            canvas?.drawColor(Color.LTGRAY)
+        }
+    }
+    
+
 }
